2d0f9ee15d220470a3a54018ed5ba66b8c7b3cc9
Ming Duan <mingduan64@gmail.com>
2022-02-12 01:28:58 +0700

    ProductCatalog svc /listener/categoryDeleteEvent v_1

--- product-catalog-service/src/main/java/com/example/productcatalogservice/controllers/listener/CatalogCreateEventController.java
+++ product-catalog-service/src/main/java/com/example/productcatalogservice/controllers/listener/CatalogCreateEventController.java
@@ -1,22 +1,13 @@
 package com.example.productcatalogservice.controllers.listener;
 
-import com.example.productcatalogservice.api.ApiUtil;
 import com.example.productcatalogservice.models.CatalogCreateEvent;
-import com.example.productcatalogservice.models.EventSubscription;
-import com.example.productcatalogservice.repositories.listener.CatalogCreateEventRepository;
-import com.example.productcatalogservice.services.listener.CatalogAttributeValueChangeEventService;
 import com.example.productcatalogservice.services.listener.CatalogCreateEventService;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/tmf-api/productCatalogManagement/v4/listener/catalogCreateEvent")
