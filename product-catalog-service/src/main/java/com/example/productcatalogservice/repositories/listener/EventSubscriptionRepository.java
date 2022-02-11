a692faafb204af38a2c75bd7b85856946f813a28
Ming Duan <mingduan64@gmail.com>
2022-02-11 16:27:43 +0700

    product-catalog-service /listener/catalogCreateEvent v_1

---
+++ product-catalog-service/src/main/java/com/example/productcatalogservice/repositories/listener/EventSubscriptionRepository.java
@@ -0,0 +1,8 @@
+package com.example.productcatalogservice.repositories.listener;
+
+import com.example.productcatalogservice.models.CatalogCreateEvent;
+import com.example.productcatalogservice.models.EventSubscription;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EventSubscriptionRepository extends MongoRepository<EventSubscription,String> {
+}
