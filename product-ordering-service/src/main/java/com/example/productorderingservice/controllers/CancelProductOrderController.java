48372f12081a39f49d8c3590e5b24238d138f84f
Ming Duan <mingduan64@gmail.com>
2022-02-28 18:22:05 +0700

    product-ordering /cancelProductOrder v_1

--- product-ordering-service/src/main/java/com/example/productorderingservice/controllers/CancelProductOrderController.java
+++ product-ordering-service/src/main/java/com/example/productorderingservice/controllers/CancelProductOrderController.java
@@ -1,21 +1,43 @@
-//package com.example.productorderingservice.controllers;
-//
-//import com.example.productcatalogservice.api.CancelProductOrderApi;
-//import com.example.productcatalogservice.models.CancelProductOrder;
-//import com.example.productorderingservice.services.CancelProductOrderService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class CancelProductOrderController implements CancelProductOrderApi {
-//    @Autowired
-//    private CancelProductOrderService cancelProductOrderService;
-//    @Override
-//    public ResponseEntity<CancelProductOrder> createCancelProductOrder(){
-//        CancelProductOrder savedCancelProductOrder = cancelProductOrderService.save(cancelProductOrder);
-//        return ResponseEntity.ok().body(savedCancelProductOrder);
-//    }
-//
-//}
+package com.example.productorderingservice.controllers;
+
+import com.example.productorderingservice.models.CancelProductOrder;
+import com.example.productorderingservice.models.CancelProductOrderCreate;
+import com.example.productorderingservice.repositories.CancelProductOrderRepository;
+import com.example.productorderingservice.services.CancelProductOrderCreateService;
+import com.example.productorderingservice.services.CancelProductOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tmf-api/productOrderingManagement/v4/cancelProductOrder")
+public class CancelProductOrderController {
+    @Autowired
+    private CancelProductOrderService cancelProductOrderService;
+    @Autowired
+    private CancelProductOrderCreateService cancelProductOrderCreateService;
+
+
+    @GetMapping
+    public List<CancelProductOrder> getAll() {
+        return cancelProductOrderService.getAll();
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CancelProductOrderCreate> save(CancelProductOrderCreate cancelProductOrder) {
+        CancelProductOrderCreate savedCancelProductOrderCreate = cancelProductOrderCreateService.createCancelProductOrder(cancelProductOrder);
+        return ResponseEntity.ok().body(savedCancelProductOrderCreate);
+    }
+
+
+
+    @GetMapping("/{id}")
+    public String getCancelProductOrderById(@PathVariable String id){
+        cancelProductOrderService.getCancelProductOrderbyId(id);
+        return "Found Cancel Product Successfully";
+    }
+
+}
--- product-ordering-service/src/main/java/com/example/productorderingservice/services/impl/CancelProductOrderServiceImpl.java
+++ product-ordering-service/src/main/java/com/example/productorderingservice/services/impl/CancelProductOrderServiceImpl.java
@@ -1,33 +1,30 @@
-//package com.example.productorderingservice.services.impl;
-//
-//import com.example.productorderingservice.models.CancelProductOrder;
-//import com.example.productorderingservice.models.CancelProductOrderCreate;
-//import com.example.productorderingservice.repositories.CancelProductOrderCreateRepository;
-//import com.example.productorderingservice.repositories.CancelProductOrderRepository;
-//import com.example.productorderingservice.services.CancelProductOrderService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//public class CancelProductOrderServiceImpl implements CancelProductOrderService {
-//    @Autowired
-//    private CancelProductOrderRepository repository;
-//    @Autowired
-//    private CancelProductOrderCreateRepository repository1;
-//
-////    @Override
-////    public List<CancelProductOrder> getAll() {
-////        return repository.findAll();
-////    }
-////
-////    @Override
-////    public CancelProductOrder save(CancelProductOrder cancelProductOrder) {
-////        return repository.save(cancelProductOrder);
-////    }
+package com.example.productorderingservice.services.impl;
+
+import com.example.productorderingservice.models.CancelProductOrder;
+import com.example.productorderingservice.repositories.CancelProductOrderRepository;
+import com.example.productorderingservice.services.CancelProductOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CancelProductOrderServiceImpl implements CancelProductOrderService {
+    @Autowired
+    private CancelProductOrderRepository repository;
+
+    @Override
+    public List<CancelProductOrder> getAll() {
+        return repository.findAll();
+    }
+    @Override
+    public Optional <CancelProductOrder> getCancelProductOrderbyId(String id) {
+        return repository.findById(id);
+    }
 //    @Override
-//    @Transactional
-//    public CancelProductOrder createCancelProductOrder(CancelProductOrder cancelProductOrder) {
+//    public CancelProductOrder save(CancelProductOrder cancelProductOrder) {
 //        return repository.save(cancelProductOrder);
 //    }
-//}
+}
